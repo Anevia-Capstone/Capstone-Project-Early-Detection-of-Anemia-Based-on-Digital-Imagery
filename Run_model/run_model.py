f55ee815252375af0dd0a22f632f78b823e8e428
Yywtv <150881064+Yywtv@users.noreply.github.com>
2025-06-12 16:55:31 +0700

    Update run_model.py

--- Run_model/run_model.py
+++ Run_model/run_model.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+from PIL import Image
 
 # establish available classes for prediction and image size for input shape
 class_names = ["Anemic", "Non-Anemic"]
@@ -11,11 +12,14 @@ model = load_model("/path/to/model.h5") # adjust path based on model location
 
 # Process the input image prior to model inference
 def load_and_preprocess_image(path):
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, img_size)
-    img = img / 255.0
-    return np.expand_dims(img, axis=0), img
+    with Image.open(path).convert("RGBA") as img:
+        bbox = img.getbbox()
+        img_cropped = img.crop(bbox) if bbox else img
+        background = Image.new("RGB", img_cropped.size, (255, 255, 255))
+        img_rgb = Image.alpha_composite(background.convert("RGBA"), img_cropped).convert("RGB")
+        img_resized = img_rgb.resize(img_size)
+        img_array = np.array(img_resized) / 255.0
+        return np.expand_dims(img_array, axis=0), np.array(img_rgb)
 
 input_img, display_img = load_and_preprocess_image("/path/to/image.jpeg") # adjust path based on image location
 prediction = model.predict(input_img) # output prediction in percentage; ex. [[0.37858227 0.57706666]] : first index is Anemic, second index is Non-Anemic
